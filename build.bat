0885d43c951f7755e169b89144e2a1ec11f4d649
CI-DEV <154627941+IlumCI@users.noreply.github.com>
2024-06-09 13:59:44 +0300

    Linuxified

--- build.bat
+++ build.bat
@@ -3,13 +3,13 @@ color 4
 setlocal EnableDelayedExpansion
 title Looking for Python...
 
+:: Check if Python is installed
 where python >nul 2>nul
-if errorlevel 1 (
-    echo Python is not installed. Please install it over this link, but also make sure to add it to PATH. Then restart this file.
+if %errorlevel% neq 0 (
+    echo Python is not installed. Please install it from the following link, and make sure to add it to PATH. Then restart this file.
     echo https://www.python.org/ftp/python/3.11.6/python-3.11.6-amd64.exe
-
     pause
-    exit
+    exit /b
 ) else (
     echo Python is installed.
 )
@@ -24,7 +24,7 @@ call rosevenv\Scripts\activate
 
 title Installing packages...
 echo Installing packages...
-echo This may take a while. Be pacient!
+echo This may take a while. Be patient!
 python -m pip install --upgrade --ignore-installed -r resources\data\requirements.txt
 
 title Starting builder...
